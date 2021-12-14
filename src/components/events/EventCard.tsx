e99e8a8d1a6c6a741505424b57ee2c29e79a7804
Sandra Lindström <sandra.lindstrm@gmail.com>
2021-12-14 14:50:08 +0100

    Added event data json files, Added Event Component, Added EventCard Component

---
+++ src/components/events/EventCard.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import { Events as EventsModel } from '../../models/Events'
+
+function EventCard(props: { event_item: EventsModel, eventCallback: (item_data:EventsModel) => void }) {
+  const event_item = props.event_item
+  const image = event_item.image ? event_item.image : 'logo192.png'
+  const [title, setTitle] = useState(event_item.name)
+
+  let place = event_item.location.city ? event_item.location.city : event_item.location.other
+  place = place && place.charAt(0).toUpperCase() + place.slice(1)
+
+
+useEffect(()=>{
+ if (event_item.name.length > 24) {
+    let short_name = event_item.name.match(/[\s\S]{1,22}/g) || [] 
+    setTitle(short_name[0].trim() + '...')
+  }
+}, [event_item.name])
+ 
+
+  return (
+    <>
+      <section className="event-card">
+        <h2>{title}</h2>
+        <div
+          className="image-container"
+          data-test="image-container"
+          style={{ backgroundImage: `url("/${image}")` }}
+        >
+        </div>
+         <div>
+            <span data-test="event-date">{event_item.date}</span>
+            <span data-test="event-location">{place && place}</span>
+           </div>
+        <div>
+          <button data-test="read-more-btn" onClick={()=>(props.eventCallback(event_item))}>Read more</button>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default EventCard
---
+++ src/components/events/Events.tsx
@@ -0,0 +1,24 @@
+import { useContext } from "react"
+import LoginContext from "../../contexts/LoginContext"
+import { Events as EventsModel } from '../../models/Events'
+
+import EventCard from "./EventCard"
+import './style.scss'
+function Events(props: { events: Array<object> , singleEventsCallback: (obj:EventsModel)=>void}) {
+  function handleClick(event_data:EventsModel) {
+    console.log('Event Callback')
+    return props.singleEventsCallback(event_data)
+  }
+
+  return (
+    <>
+      <section className="events-wrapper">
+        {props.events.map((item: any) => (
+          <EventCard key={item.id} event_item={item} eventCallback={handleClick}/>
+        ))}
+      </section>
+    </>
+  )
+}
+
+export default Events
