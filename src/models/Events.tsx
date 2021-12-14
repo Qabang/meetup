cceabb16236ad7a7f1d177ed7a12888af9d83f73
Sandra Lindström <sandra.lindstrm@gmail.com>
2021-12-14 14:52:03 +0100

    Added Events Model

---
+++ src/models/Events.tsx
@@ -0,0 +1,15 @@
+export interface Events {
+  id: number
+  name: string
+  date: string
+  time_start: string
+  time_end: string
+  description: string
+  participants: number
+  image?: string
+  location: {
+    city?: string
+    adress?: string
+    other?: string
+  }
+}
