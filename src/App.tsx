658ba6d769bf5f9872581fd6d02dff451f47ff2c
Sandra Lindström <sandra.lindstrm@gmail.com>
2021-12-14 14:55:51 +0100

    Refactor due to added login context and routing by useNavigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,53 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
 import {Users} from './models/Users'
 import Login from './components/Login'
+import Events from './components/events/Events'
+import { Events as EventsModel } from './models/Events'
+
+
+import EventsData from './components/events/events-data.json'
 import './App.css'
 
 import {
-  BrowserRouter as Router,
   NavLink,
   Route,
   Routes,
+  useNavigate,
 } from 'react-router-dom'
+import LoginContext from './contexts/LoginContext'
+import EventItem from './components/EventItem'
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [activeUser, setActiveUser] = useState<Users>({username: '', password:'', role:''})
+  const [activeUser, setActiveUser] = useState<Users>({username: '', role:'', events:[]})
+  const [eventData, setEventData ] = useState<EventsModel | null>(null)
+   
+  let navigate = useNavigate();
+
+  useEffect(()=>{
+    if (eventData !== null) {
+      navigate(`/event/${eventData.id}`, { replace: true });
+    }
+  }, [eventData])
 
   function handleIsLoggedIn(bool: boolean, user: any) {
     setIsLoggedIn(bool)
     setActiveUser(user)
   }
 
+  function handleSingleEventDetails(event_data:any) {
+    console.log(event_data)
+    return setEventData(event_data)
+  }
+
   return (
     <div className="App">
-      <Router>
+      <LoginContext.Provider value={activeUser}>
         <header className="App-header">
           <nav>
             <NavLink to="/"> Start</NavLink> |
+            {activeUser.username}
           </nav>
         </header>
         <section className="main-content">
@@ -37,11 +60,15 @@ function App() {
               )}
               {isLoggedIn && (<Route
                 path="/"
-                element={<div>Hello {activeUser.username}</div>}
+                element={<Events events={EventsData} singleEventsCallback={handleSingleEventDetails}/>}
               ></Route>)}
+              <Route
+                path="/event/:id"
+                element={<EventItem data={eventData}/>}
+              ></Route>
           </Routes>
         </section>
-      </Router>
+      </LoginContext.Provider>
     </div>
   )
 }
